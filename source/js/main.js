20e4ee8d6bcf73dd2dec038c2022c6b402e3e17f
fender5234 <fender5234@gmail.com>
2023-01-10 23:53:52 +0300

    Стили Десктоп Планшет

--- source/js/main.js
+++ source/js/main.js
@@ -1,7 +1,55 @@
+import initMaps from './modules/map.js';
+
 // в load следует добавить скрипты, не участвующие в работе первого экрана
 window.addEventListener('load', () => {
-});
+  window.pageData = {
+    maps: {
+      map: {
+        initials: {
+          center: [59.9387165, 30.3230474],
+          controls: [],
+          zoom: 16,
+        },
+        placemark: [
+          {
+            hintContent: 'г. Санкт Петербург, ул. Большая Конюшенная, 19/8',
+          },
+          {
+            iconImageHref: 'img/sprite.svg#icon-marker',
+            iconImageSize: [18, 22],
+            iconLayout: 'default#image',
+            iconShadow: false,
+          },
+        ],
+      },
+    },
+  };
+  const initLazy = () => {
+    if (window.pageYOffset <= window.innerHeight) {
+      return;
+    }
 
+    initMaps(document.querySelectorAll('[data-map][id]'));
+    // document.querySelectorAll('[data-form]').forEach(initForm);
+
+    document.querySelectorAll('[data-lazy-style]').forEach((lazyStyledElement) => {
+      lazyStyledElement.setAttribute('style', lazyStyledElement.dataset.lazyStyle);
+      lazyStyledElement.removeAttribute('data-lazy-style');
+    });
+
+    document.removeEventListener('scroll', scrollHandler);
+  };
+
+  function scrollHandler() {
+    initLazy();
+  }
+
+  if (window.pageYOffset > window.innerHeight) {
+    initLazy();
+  } else {
+    document.addEventListener('scroll', scrollHandler);
+  }
+});
 // привязывайте js не на классы, а на дата-атрибуты (data-validate)
 
 // вместо модификаторов .block--active используем утилитарные классы
